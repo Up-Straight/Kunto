40ff64ea7206e0667f04dae36cfd4e7bedfcfe1d
MalesSebastian <malessebastian@gmail.com>
2017-05-27 07:23:25 +0300

    Owner field in day model

--- src/day/admin.py
+++ src/day/admin.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from models import Day
+
+
+class DayAdmin(admin.ModelAdmin):
+    list_display = ['owner', 'date']
+
+admin.site.register(Day, DayAdmin)
--- src/day/models.py
+++ src/day/models.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Day(models.Model):
+    owner = models.ForeignKey(User, related_name='days', on_delete=models.CASCADE, default=1)
     date = models.DateField(auto_now_add=True, editable=False)
     coefficient = models.IntegerField(null=True)
     good_standing_time = models.DurationField()
