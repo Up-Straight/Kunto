9746c33f79f91eacd6a7fd869e58334a7487d3fa
MalesSebastian <malessebastian@gmail.com>
2017-05-27 16:31:37 +0300

    Exercise change relations

--- src/exercise/models.py
+++ src/exercise/models.py
@@ -4,21 +4,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Exercise(models.Model):
-    owner = models.ForeignKey(User, related_name='exercises', on_delete=models.CASCADE, default=1)
-    name = models.CharField(max_length=50, blank=False,
-                            default='')
-    time = models.DurationField()
-
-    def __unicode__(self):
-        return self.name + " - " + self.owner.username
-
-
 class State(models.Model):
-    exercise = models.ForeignKey(Exercise, related_name='states', on_delete=models.CASCADE, default=1)
     sensor_input_one = models.IntegerField(default=0)
     sensor_input_two = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.exercise.name + " - " + \
                self.exercise.owner.username
+
+
+class Exercise(models.Model):
+    owner = models.ForeignKey(User, related_name='exercises', on_delete=models.CASCADE, default=1)
+    exercise = models.ForeignKey(State, related_name='exercise', default=1)
+
+    name = models.CharField(max_length=50, blank=False,
+                            default='')
+    time = models.DurationField()
+
+    def __unicode__(self):
+        return self.name + " - " + self.owner.username
