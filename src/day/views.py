b726e8f5f31869e6a9c347ee0ca5f2b625c007df
MalesSebastian <malessebastian@gmail.com>
2017-05-27 08:12:59 +0300

    Day views

--- src/day/views.py
+++ src/day/views.py
@@ -1,6 +1,26 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from day.models import Day
+from day.serializers import DaySerializer
+from permissionss import IsOwner
+
+
+class DayCreate(generics.ListCreateAPIView):
+    serializer_class = DaySerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Day.objects.filter(owner=user)
+
+
+class DayDetails(generics.RetrieveUpdateAPIView):
+    serializer_class = DaySerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Day.objects.filter(owner=user)
+
