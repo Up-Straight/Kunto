4acbe0efbd1e9b5652474e1d120698101d479d00
MalesSebastian <malessebastian@gmail.com>
2017-05-27 14:45:31 +0300

    Add admin classes for exercise

--- src/exercise/admin.py
+++ src/exercise/admin.py
@@ -1,6 +1,16 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from models import Exercise, State
+
+
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'owner']
+
+
+class StateAdmin(admin.ModelAdmin):
+    list_display = ['exercise']
+
+admin.site.register(Exercise, ExerciseAdmin)
+admin.site.register(State, StateAdmin)
