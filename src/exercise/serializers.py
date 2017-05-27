71db3ba60da20a2588a05a94fcafc6cb646034bf
MalesSebastian <malessebastian@gmail.com>
2017-05-27 15:09:12 +0300

    Exercise and State models serializers

---
+++ src/exercise/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+
+from exercise.models import Exercise, State
+
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    def create(self, validate_data):
+        obj = Exercise.objects.create(**validate_data)
+        obj.save()
+        return obj
+
+    class Meta:
+        model = Exercise
+        fields = ('id', 'name', 'owner')
+
+
+class StateSerializer(serializers.ModelSerializer):
+    exercise = serializers.ReadOnlyField(source='exercise.name')
+
+    def create(self, validate_data):
+        obj = State.objects.create(**validate_data)
+        obj.save()
+        return obj
+
+    class Meta:
+        model = State
+        fields = ('exercise', 'sensor_input_one',
+                  'sensor_input_two')
