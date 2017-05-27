adc861d7973ff1d7e1ce595d866bb8e48fad59ab
MalesSebastian <malessebastian@gmail.com>
2017-05-27 07:18:11 +0300

    Day model

--- src/day/models.py
+++ src/day/models.py
@@ -1,6 +1,12 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Day(models.Model):
+    date = models.DateField(auto_now_add=True, editable=False)
+    coefficient = models.IntegerField(null=True)
+    good_standing_time = models.DurationField()
+    bad_standing_time = models.DurationField()
+    total_time = models.DurationField()
+    done_training = models.BooleanField(default=False)
