492977541bf6f505c3b14b3763f0b5eee314d291
MalesSebastian <malessebastian@gmail.com>
2017-05-28 10:25:46 +0300

    Update user serializer

--- src/authentication/serializers.py
+++ src/authentication/serializers.py
@@ -9,4 +9,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'days')
+        fields = ('id', 'username', 'first_name', 'last_name', 'days')
